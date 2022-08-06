5d624d73c66a6f0968a1f334e0c444423bf46120
dobeshs <dobeshs@wwu.edu>
2022-08-05 19:27:31 -0700

    documentation update to remove full path names

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5fposix_5fc_5fsource_0',['_POSIX_C_SOURCE',['../sbuf_8c.html#a3024ccd4a9af5109d24e6c57565d74a1',1,'sbuf.c']]]
+  ['doxyreadme_2emd_0',['doxyreadme.md',['../doxyreadme_8md.html',1,'']]]
 ];
