1fc4bb6fc172b3101219b1a11f021a61207a2d50
dobeshs <dobeshs@wwu.edu>
2022-08-08 18:00:50 -0700

    updating docs

--- html/search/all_9.js
+++ html/search/all_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['new_5fsbuf_13',['new_sbuf',['../sbuf_8c.html#a38f9811bf4346e545737b0d03cf804d9',1,'new_sbuf(char *string, size_t capacity):&#160;sbuf.c'],['../sbuf_8h.html#a38f9811bf4346e545737b0d03cf804d9',1,'new_sbuf(char *string, size_t capacity):&#160;sbuf.c']]],
-  ['new_5fsbuf_5fsmall_14',['new_sbuf_small',['../sbuf_8c.html#ae43e253921f7a877cc9dc4d25608a10c',1,'new_sbuf_small(char *string):&#160;sbuf.c'],['../sbuf_8h.html#ae43e253921f7a877cc9dc4d25608a10c',1,'new_sbuf_small(char *string):&#160;sbuf.c']]]
+  ['new_5fsbuf_13',['new_sbuf',['../sbuf_8c.html#ab1bdc2c56a19ec478e30e16abff990ce',1,'new_sbuf(char *string):&#160;sbuf.c'],['../sbuf_8h.html#ab1bdc2c56a19ec478e30e16abff990ce',1,'new_sbuf(char *string):&#160;sbuf.c']]],
+  ['new_5fsbuf_5fsize_14',['new_sbuf_size',['../sbuf_8c.html#a59d456bd5473278edfc1a6e2ca54f2f5',1,'new_sbuf_size(char *string, size_t capacity):&#160;sbuf.c'],['../sbuf_8h.html#a59d456bd5473278edfc1a6e2ca54f2f5',1,'new_sbuf_size(char *string, size_t capacity):&#160;sbuf.c']]]
 ];
